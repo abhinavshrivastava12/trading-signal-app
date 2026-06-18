0fcd466a5618826a05db849fdea8db5164019485
abhinavshrivastava12 <abhinavshrivastava182@gmail.com>
2026-06-18 17:43:21 +0530

    Fix CORS for production frontend

--- backend/main.py
+++ backend/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000","https://trading-signal-frontend-s6hd.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
